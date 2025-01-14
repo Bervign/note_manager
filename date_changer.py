7b173f670634687e943f5f4e27ff0970b0031e27
Bervign <viktor.ber14@gmail.com>
2025-01-14 16:14:26 +0300

    Add files via upload

--- date_changer.py
+++ date_changer.py
@@ -1,6 +1,16 @@
+def process_string(input_string):
+    if not input_string:  # Сначала проверяется, не является ли входная строка пустой. Если пустая, функция возвращает пустую строку
+        return ""
+
+    length = len(input_string) # Вычисляется длина строки
+    if length > 8:
+        return input_string[:-5]
+    else:
+        return input_string[:-3]
+
 created_date = input('Введите дату создания заметки: ')
-print(f'дата создания, {created_date[:-5]}')
+result1 = process_string(created_date)
+print(f"дата создания:  {result1}")
 issue_date = input('Введите дедлайн заметки: ')
-print(f'дедлайн, {issue_date[:-5]}')
-
-
+result1 = process_string(issue_date)
+print(f"дедлайн:  {result1}")
