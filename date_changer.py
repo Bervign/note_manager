b46f977fcb75d8203ab52090ca495de43287ce78
Bervign <viktor.ber14@gmail.com>
2025-01-19 18:44:34 +0300

    Add files via upload

--- date_changer.py
+++ date_changer.py
@@ -1,5 +1,5 @@
 created_date = input('Введите дату создания заметки: ')
-print(f'дата создания, {created_date[:-5]}')
+print(f'дата создания, {created_date[:5]}')
 print(f'исходная дата, {created_date}')
 issue_date = input('Введите дедлайн заметки: ')
 print(f'дедлайн, {issue_date[0:8]}')
